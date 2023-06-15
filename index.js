0ddd1f078ee9df5d03ba9b6036cea93a9e0ee43c
Gus <guscdev@outlook.com>
2023-06-15 15:34:27 -0600

    First MVP created for MXN

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+// Updates to todays date
 const rateDate = document.querySelector('#rate_date')
 rateDate.valueAsDate = new Date()
+
+//Create txt file
+const form = document.querySelector('form')
+const rateId = document.querySelector('#rate_rate')
+const rateVal = document.querySelector('#rate1')
+
+form.addEventListener('submit',(evt)=>{
+    evt.preventDefault()
+    const textContent = `Hola mi nombre es gus ${rateVal.value}`
+    const blob = new Blob([textContent],{type: 'text/pain'})
+
+    const link = document.createElement('a')
+    link.href = URL.createObjectURL(blob)
+    link.download = 'rate.txt'
+
+    link.click()
+    rateVal.value = ''
+})
