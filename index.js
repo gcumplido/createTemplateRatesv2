bcc3955b6546adaab57443c5acf6f6b76c4b595f
Gus <guscdev@outlook.com>
2023-06-15 16:23:11 -0600

    One rate files working display

--- index.js
+++ index.js
@@ -2,6 +2,43 @@
 const rateDate = document.querySelector('#rate_date')
 rateDate.valueAsDate = new Date()
 
+// Change table inputs based on selecte item
+const selectInput = document.querySelector('#rate_rate')
+const from1 = document.querySelector('#from1')
+const to1 = document.querySelector('#to1')
+selectInput.addEventListener('change',(evt)=>{
+    const rateFile = selectInput.value
+    if (rateFile === 'ARSA' || rateFile === 'ARSB'){
+        from1.innerHTML = 'USD'
+        to1.innerHTML = 'ARS'
+    }
+    if (rateFile === 'CLF'){
+        from1.innerHTML = 'CLF'
+        to1.innerHTML = 'CLP'
+    }
+    if (rateFile === 'COP'){
+        from1.innerHTML = 'USD'
+        to1.innerHTML = 'COP'
+    }
+    if (rateFile === 'MXN'){
+        from1.innerHTML = 'USD'
+        to1.innerHTML = 'MXN'
+    }
+    if (rateFile === 'NGN'){
+        from1.innerHTML = 'USD'
+        to1.innerHTML = 'NGN'
+    }
+    if (rateFile === 'PENA' || rateFile === 'PENB'){
+        from1.innerHTML = 'USD'
+        to1.innerHTML = 'PEN'
+    }
+    if (rateFile === 'VND'){
+        from1.innerHTML = 'VND'
+        to1.innerHTML = 'USD'
+    }
+
+})
+
 //Create txt file
 const form = document.querySelector('form')
 const rateId = document.querySelector('#rate_rate')
