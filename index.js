6794f6e69fa2798923ebc3ecc6e8b1cd2956755c
Gus <guscdev@outlook.com>
2023-06-15 18:12:09 -0600

    Complete table with 2 currencies

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ rateDate.valueAsDate = new Date()
 const selectInput = document.querySelector('#rate_rate')
 const from1 = document.querySelector('#from1')
 const to1 = document.querySelector('#to1')
+const from2 = document.querySelector('#from2')
+const to2 = document.querySelector('#to2')
 const divRate2 = document.querySelector('.rowRate2')
 selectInput.addEventListener('change',(evt)=>{
     const rateFile = selectInput.value
@@ -45,6 +47,25 @@ selectInput.addEventListener('change',(evt)=>{
         case 'VND':
             from1.innerHTML = 'VND'
             to1.innerHTML = 'USD'
+            break
+        // Case with two rates
+        case 'BRL':
+            from1.innerHTML = 'BRL'
+            to1.innerHTML = 'USD'
+            from2.innerHTML = 'BRL'
+            to2.innerHTML = 'EUR'
+            break
+        case 'IDR':
+            from1.innerHTML = 'SGD'
+            to1.innerHTML = 'IDR'
+            from2.innerHTML = 'USD'
+            to2.innerHTML = 'IDR'
+            break
+        case 'MAD':
+            from1.innerHTML = 'USD'
+            to1.innerHTML = 'MAD'
+            from2.innerHTML = 'EUR'
+            to2.innerHTML = 'MAD'
             break
     }
 })
